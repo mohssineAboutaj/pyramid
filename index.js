37d2fe030a2af53597a10dd40666dcf6d09c8694
mohssineAboutaj <mohssineaboutaj1995@gmail.com>
2021-01-07 03:20:21 +0100

    convert code from FN to OOP

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
-// variables & options
-const middleStone = "x".repeat(60);
-const toSlice = 2;
-const start = (middleStone.length - 1) / (toSlice > 2 ? toSlice : 2);
-const zigZagIn = middleStone.length / 10;
+class Pyramid {
+  /**
+   * stones count to slice from both edge
+   */
+  toSlice = 2;
 
-/**
- * @description stones trimer
- *
- * @param {String} stones
- *
- * @returns trimed stones line
- */
-function stoneTrimer(stones) {
-  // convert to Array
-  stones = Array.from(stones);
+  /**
+   * @description stones trimer
+   *
+   * @param {String} stones
+   *
+   * @returns trimed stones line
+   */
+  stoneTrimer(stones) {
+    // convert to Array
+    stones = Array.from(stones);
 
-  // delete edge
-  delete stones[stones.length - 1];
-  delete stones[0];
+    // delete edge
+    delete stones[stones.length - 1];
+    delete stones[0];
 
-  // convert to String
-  return stones.toString().replace(/,/g, "");
-}
+    // convert to String
+    return stones.toString().replace(/,/g, "");
+  }
 
-/**
- * @description the pyramid builder
- *
- * @param {String} stones
- * @param {Number} j
- *
- * @returns line stones with edge
- */
-function pyramidBuilder(stones, j) {
-  return " ".repeat(j) + "/" + stoneTrimer(stones) + "\\";
-}
+  /**
+   * @description the pyramid builder
+   *
+   * @param {String} stones
+   * @param {Number} j
+   *
+   * @returns line stones with edge
+   */
+  pyramidBuilder(stones, j) {
+    return " ".repeat(j) + "/" + this.stoneTrimer(stones) + "\\";
+  }
 
-// show output
-for (let i = start; i > 0; i--) {
-  if (zigZagIn > 1 && toSlice >= 2) {
-    if (!(i % zigZagIn > zigZagIn - 1)) {
-      console.log(pyramidBuilder(middleStone.slice(i * toSlice), i));
+  // show output
+  constructor(middleStone, zigZagIn = 1, toSlice = this.toSlice) {
+    for (
+      let i = (middleStone.length - 1) / (toSlice > 2 ? toSlice : 2);
+      i > 0;
+      i--
+    ) {
+      if (zigZagIn > 1 && toSlice >= 2) {
+        if (!(i % zigZagIn > zigZagIn - 1)) {
+          console.log(this.pyramidBuilder(middleStone.slice(i * toSlice), i));
+        }
+      } else {
+        console.log(this.pyramidBuilder(middleStone.slice(i * toSlice), i));
+      }
     }
-  } else {
-    console.log(pyramidBuilder(middleStone.slice(i * toSlice), i));
   }
 }
+
+// set variables & create new instance to build the pyramid
+const middleStone = "x".repeat(60);
+new Pyramid(middleStone, middleStone.length / 10);
